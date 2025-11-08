d32a62fe161ff4a79679283936c99ac5edbb98af
Igor Jerônimo <igorjeronimo2007@gmail.com>
2025-11-07 22:15:38 -0300

    Desenvolvimento das funções de limites
    Já é possível resolver limites laterais e indeterminados.
    
    Se atentar apenar para a gramática de escrita das funções.

---
+++ Av1 Calculo A/limites.py
@@ -0,0 +1,26 @@
+import sympy as sp
+
+def calcular_limites():
+    x = sp.symbols('x') # Definindo a variável simbólica
+    f = input("Digite a função: ")# Função a ser analisada
+    i = input('Digite o limite da função (use "oo" para infinito): ') # Ponto onde o limite será calculado
+
+    limite = sp.limit(f,x,i) # Calculando o limite
+    return(f'O limite de {f} quando x tende a {i} é: {limite}')
+
+
+def limites_laterais():
+    x = sp.symbols('x')
+    f = input("Digite a função: ")
+    i = input('Digite o limite da função (use "oo" para infinito): ')
+
+    limite_direita = sp.limit(f,x,i, dir='+') # Calculando o limite pela direita
+    limite_esquerda = sp.limit(f,x,i, dir='-') # Calculando o limite pela esquerda 
+
+    
+    return(f'''O limite de {f} quando x tende a {i} para a DIREITA é: {limite_direita}
+O limite da de {f} quando x tende a {i} para a ESQUERDA é: {limite_esquerda}''')
+
+    
+
+print(limites_laterais())
